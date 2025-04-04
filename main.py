01ef196662cce39a5469fa57052f57e315032512
Singh <Navamandeep.Singh@ised-isde.gc.ca>
2025-04-04 11:12:04 -0400

    Added 2nd line to code

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print("Added a 2nd line to print")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
